fd47f61957c82e24002e240125364983fddd4155
Tushar656 <ui20cs69@iiitsurat.ac.in>
2023-08-05 00:21:56 +0530

    make https

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://social-vista-server.glitch.me/api/",
+  baseURL: "https://social-vista-server.glitch.me/api/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
