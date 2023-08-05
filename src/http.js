e217b6377611eb3a3a12c104afbe4b82db5c1dfc
Tushar656 <ui20cs69@iiitsurat.ac.in>
2023-08-05 18:33:30 +0530

    fix server error

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "API/",
+  baseURL: "https://social-vista-server.glitch.me/API/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
